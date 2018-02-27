7db32eed3ec007ddb15a4e68fdc1b56cfb9bc6cc
DomFabian <dominickfabian@gmail.com>
2018-02-27 10:48:55 -0600

    Start index actions

--- web/index.php
+++ web/index.php
@@ -10,6 +10,9 @@
 **   This file contains the index landing page for the
 ** 315 project 1. All GET and POST requests coming from
 ** the PedestrianCounter will be handled by this script.
+** This file relies on the fact that the Arduino can send a 
+** specially-crafted HTTP POST request to the server that
+** no on else will know how to create.
 **
 ***********************************************/
 
@@ -17,8 +20,20 @@
 
     $tableName = '`ArduinoTest`';
 
-    $test = insertIntoDatabase($tableName);
-    var_dump($test);
-    echo "<br><br>";
-    echo getNumDatabaseEntries($tableName);
+    if ($_SERVER["REQUEST_METHOD"] === "GET") {
+        // if just visiting site, not sending data to it
+        
+        // TODO: define action; probs just some HTML filler
+    }
+    else if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        // if attempting to send data to server
+
+        // TODO: perform test to see how a POST request should
+        //       be formatted as if the server had sent a form
+        //       to request the key from the Arudino client!
+        //          Maybe use BurpSuite proxy?
+
+        $receivedKey = $_POST["key"];
+        $statusCode = handleArduinoPing($receivedKey);
+    }
 ?>
